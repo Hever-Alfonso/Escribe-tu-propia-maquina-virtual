7a0edaa96adfed655306aad8eb50de0823936a71
Hever Alfonso <haalfonsoj@eafit.edu.co>
2025-10-03 09:34:27 -0500

    Definir registros del LC-3 (R0-R7, PC, COND)

--- lc3.c
+++ lc3.c
@@ -1,2 +1,19 @@
 #define MEMORY_MAX (1 << 16)
+
 uint16_t memory[MEMORY_MAX]; /* 65536 posiciones */
+
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, /* contador de programa */
+    R_COND, /* registro de condiciones */
+    R_COUNT, /* cantidad de registros */
+};
