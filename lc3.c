24b277877efa8eb12ab6f225f84eb54c4f9edfa0
Hever Alfonso <haalfonsoj@eafit.edu.co>
2025-10-03 12:28:51 -0500

    Implementada fase FETCH: lectura de instrucción desde memoria, incremento del PC y extracción del opcode con comentarios en español

--- lc3.c
+++ lc3.c
@@ -50,3 +50,78 @@ enum
     FL_ZRO = 1 << 1, /* Z: cero */
     FL_NEG = 1 << 2, /* N: negativo */
 };
+
+int main (int argc, const char* argv[])
+{
+    /* Cargar argumentos */
+    /* Configuración inicial */
+
+    /* Como exactamente un flag de condición debe estar activo en cada momento,
+        inicializamos el flag Z (cero) */
+    reg[R_COND] = FL_ZRO;
+
+    /* Establecer el PC en la posición incial */
+    /* 0x3000 es la posición de inicio por defecto en LC-3 */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH: obtener instrucción */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12; // obtenemos el opcode
+
+        switch (op)
+        {
+            case OP_ADD:
+                /* ejecutar ADD */
+                break;
+            case OP_AND:
+                /* ejecutar AND */
+                break;
+            case OP_NOT:
+                /* ejecutar NOT */
+                break;
+            case OP_BR:
+                /* ejecutar salto condicional (BR) */
+                break;
+            case OP_JMP:
+                /* ejecutar salto (JMP) */
+                break;
+            case OP_JSR:
+                /* ejecutar salto a subrutina (JSR) */
+                break;
+            case OP_LD:
+                /* ejecutar load (LD) */
+                break;
+            case OP_LDI:
+                /* ejecutar load indirect (LDI) */
+                break;
+            case OP_LDR:
+                /* ejecutar load register (LDR) */
+                break;
+            case OP_LEA:
+                /* ejecutar load effective address (LEA) */
+                break;
+            case OP_ST:
+                /* ejecutar store (ST) */
+                break;
+            case OP_STI:
+                /* ejecutar store indirect (STI) */
+                break;
+            case OP_STR:
+                /* ejecutar store register (STR) */
+                break;
+            case OP_TRAP:
+                /* ejecutar llamada al sistema (TRAP) */
+                break;
+            case OP_RES:   // reservado
+            case OP_RTI:   // retorno de interrupción
+            default:
+                /* BAD OPCODE → código inválido */
+                break;
+        }
+    }
+    /* Apagar o terminar */
+}
